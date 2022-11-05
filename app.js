b1b543756a2735f9d333151b82354d05e19a3141
César Higashi <76760288+higashi04@users.noreply.github.com>
2022-11-05 16:04:00 -0500

    added user route to app, configured session

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ const bodyParser = require("body-parser");
 const MongoStore = require("connect-mongo");
 const mongoose = require("mongoose");
 
+const usersRoutes = require("./Routes/user");
+
 const corsOptions = {
-    origin: (origin, callback) => {
-        if(whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error());
-        }
-    },
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error());
+    }
+  },
 };
 
 app.use(cors());
@@ -23,26 +25,44 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 const store = MongoStore.create({
-    mongoUrl: process.env.MONGO_SERVER,
-    touchAfter: 24 * 3600,
-    crypto: {
-        secret: process.env.SECRET,
-    },
+  mongoUrl: process.env.MONGO_SERVER,
+  touchAfter: 24 * 3600,
+  crypto: {
+    secret: process.env.SECRET,
+  },
 });
 
-store.on("error", (e)=> {
-    console.log(e)
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24,
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
+
+store.on("error", (e) => {
+  console.log(e);
 });
 mongoose.connect(process.env.MONGO_SERVER);
 mongoose.connection.on("connected", () => {
-    console.log("mongo atlas connected")
-})
+  console.log("mongo atlas connected");
+});
 mongoose.connection.on("error", (err) => {
-    console.error("mongo error: \n", err)
-})
+  console.error("mongo error: \n", err);
+});
+app.use(cookieParser(process.env.SECRET));
+app.use("/usuario", usersRoutes);
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => [
-    console.log(`app running in port ${port}`)
-])
+app.get("/", (req, res) => {
+    res.send("backend app working")
+    console.log("backend app connected")
+}); 
+
+app.listen(port, () => [console.log(`app running in port ${port}`)]);
